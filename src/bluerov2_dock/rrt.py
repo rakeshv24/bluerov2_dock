525b91a09a50a6100fcc8ba0840fc97bff36afdd
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2022-10-28 09:27:31 -0700

    loaded occ_map into planner. [TODO] fix the planner

--- src/bluerov2_dock/rrt.py
+++ src/bluerov2_dock/rrt.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import cKDTree
 import time
+from tqdm import tqdm
 
 
 class RRT:
@@ -14,21 +15,32 @@ class RRT:
             self.y = y
             self.parent = None
 
-    def __init__(self, start, goal):
-        self.start = self.Node(start)
-        self.goal = self.Node(goal)
-        self.print_verbose()
-        self.bounds = np.array([-50.0, -50.0])
+    def __init__(self):
+        self.filename = "/Users/rakeshvivekanandan/workspace/bluerov2_dock/data/occ_map_binary.npy"
+        self.load_map()
+        self.bounds = np.array([[0, self.occ_map.shape[0]], [0, self.occ_map.shape[1]]])
         # self.min_rand = 0
         # self.max_rand = 1000
-        self.step_size = 0.5
+        self.step_size = 0.25
         self.goal_sample_rate = 0.1
         self.max_nodes = 10000
         self.node_list = []
+        
+    def initialize_states(self, start, goal):
+        self.start = self.Node(start[0], start[1])
+        self.goal = self.Node(goal[0], goal[1])
+        self.print_verbose()
+
+    def load_map(self):
+        self.occ_map = np.load(self.filename)
+        # print(self.occ_map.shape)
+        fig = plt.figure()
+        plt.imshow(self.occ_map.T, cmap="binary")
+        plt.show()
 
     def print_verbose(self):
-        print('Start: {}'.format(self.start_state))
-        print('Goal: {}'.format(self.goal_state))
+        print('Start: {}, {}'.format(self.start.x, self.start.y))
+        print('Goal: {}, {}'.format(self.goal.x, self.goal.y))
         
     def check_hit(self, start, goal):
         """
@@ -40,11 +52,11 @@ class RRT:
         dy = gy - y
 
         # Went off the maze
-        if (x < self.bounds[0]) or (y < self.bounds[0]) or (x >= self.bounds[1]) or (y >= self.bounds[1]):
+        if (x < self.bounds[0][0]) or (y < self.bounds[1][0]) or (x >= self.bounds[0][1]) or (y >= self.bounds[1][1]):
             return True
 
         # Starting in an obstacle
-        if self.maze_array[int(round(start[0])), int(round(start[1]))] == 0:
+        if self.occ_map[int(round(start.x)), int(round(start.y))] == 1.0:
             return True
 
         if dx == 0.0 and dy == 0.0: # we don't actually move, so we are done
@@ -60,10 +72,10 @@ class RRT:
             x += dx
             y += dy
             # Went off the maze
-            if (x < 0) or (y < 0) or (x >= self.cols) or (y >= self.rows):
+            if (x < self.bounds[0][0]) or (y < self.bounds[1][0]) or (x >= self.bounds[0][1]) or (y >= self.bounds[1][1]):
                 return True
             # Went into collision cell
-            if self.maze_array[int(x),int(y)] == 0:
+            if self.occ_map[int(x),int(y)] == 1.0:
                 return True
         return False
 
@@ -82,7 +94,7 @@ class RRT:
             
             if not collision_check:
                 self.node_list.append(new_node)
-
+                
             if (new_node.x, new_node.y) == (self.goal.x, self.goal.y):
                 print("Runtime: ", (time.time() - start_time))
                 return self.generate_path()
@@ -92,6 +104,7 @@ class RRT:
                 print("Runtime: ", (time.time() - start_time))
                 return self.generate_path()
             else:
+                # print((self.node_list[-1].x, self.node_list[-1].y))
                 continue
 
         return None
@@ -131,8 +144,8 @@ class RRT:
 
     def get_random_node(self):
         if np.random.rand() > self.goal_sample_rate:
-            rand = self.Node(random.uniform(self.bounds[0], self.bounds[1]),
-                             random.uniform(self.bounds[0], self.bounds[1]))
+            rand = self.Node(random.uniform(self.bounds[0][0], self.bounds[0][1]),
+                             random.uniform(self.bounds[1][0], self.bounds[1][1]))
         else:
             rand = self.Node(self.goal.x, self.goal.y)
         return rand
@@ -150,6 +163,7 @@ class RRT:
         else:
             path = self.planning()
             if path is not None:
+                # print("Path: ", path)
                 path_length = 0
                 for u in range(1, len(path) - 1):
                     path_length += math.sqrt((path[u][0] - path[u - 1][0]) ** 2 + (path[u][1] - path[u - 1][1]) ** 2)
@@ -158,4 +172,5 @@ class RRT:
 
 if __name__ == '__main__':
     obj = RRT()
-    obj.execute()
+    obj.initialize_states((775, 775), (1015, 680))
+    # obj.execute()
